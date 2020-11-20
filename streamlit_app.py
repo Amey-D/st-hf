a8ca1cbfc494b83c1c944f8809b9348448483daa
Amey Deshpande <amey@streamlit.io>
2020-11-19 22:52:36 -0800

    Update the app to use duration and limit number of tweets to analyze

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,28 +13,24 @@ st.title("Streamlit Twitter Feed Analyzer")
 with st.sidebar.beta_expander("Twitter Dataset", expanded=True):
     token_from_env = os.environ['TWITTER_API_TOKEN']
     token = token_from_env if token_from_env else st.text_input("Enter Twitter API bearer token")
-    search_keyword = st.text_input("Enter a search keyword", value="#streamlit")
+    search_keyword = st.text_input("Enter a search keyword", value="")
     tweet_language = st.selectbox("Select language", options=["en"], index=0)
-    max_count = st.slider("Max number ot tweets", min_value=10, max_value=100, value=50, step=10)
     include_retweets = st.radio("Include retweets", options=[False, True])
     fields = st.multiselect(
         "Fields to show",
         options=["source", "created_at", "lang", "referenced_tweets"])
-    zero_shot_categories = st.text_input(
-        "Comma-separated categories to use for zero-shot classification",
-        "finance,medicine,politics,machinelearning,sports"
-    )
+    duration = st.selectbox(
+        "Duration of tweets",
+        options=["Last 4 hours", "Last 8 hours", "Last 24 hours", "Last 48 hours", "Last 72 hours"])
+    max_count = st.slider("Max number tweets to analyze", min_value=10, max_value=500, value=50, step=10)
 
 fetch_and_analyze_tweets(
     token=token,
     search_keyword=search_keyword,
     fields=fields,
     tweet_language=tweet_language,
-    max_count=max_count,
     include_retweets=include_retweets,
-    zero_shot_categories=zero_shot_categories.split(','),
+    duration=duration,
+    max_count=max_count,
 )
-
-#import streamlit.components.v1 as components
-#components.iframe('<blockquote class="twitter-tweet"><p lang="en" dir="ltr">just setting up my twttr</p>&mdash; jack (@jack) <a href="https://twitter.com/jack/status/20?ref_src=twsrc%5Etfw">March 21, 2006</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>')
     
--- twitter.py
+++ twitter.py
@@ -1,5 +1,6 @@
 from collections import Counter
 from datetime import datetime, timedelta
+import time
 from urllib import parse
 
 import pandas as pd
@@ -8,10 +9,9 @@ import streamlit as st
 import streamlit.components.v1 as components
 from transformers import pipeline
 
-
-# @st.cache(allow_output_mutation=True, ttl=60*60)
-def _get_recent_tweets(token, search_keyword, fields, lang='en', include_retweets=False, max_count=100):
-    print(f'Getting up to {max_count} recent tweets ')
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+def _get_all_tweets(token, search_keyword, fields, duration):
+    print('Fetching recent tweets ...')
     fields_ = set(['text', 'referenced_tweets', 'lang'] + fields)
     # end_time = datetime.now()
     # start_time = (end_time - timedelta(hours=24))
@@ -21,23 +21,25 @@ def _get_recent_tweets(token, search_keyword, fields, lang='en', include_retweet
         'query': search_keyword,
         'max_results': 100,
         'tweet.fields': ','.join(fields_),
-        # 'start_time': '2020-11-18T00:00:00Z',
-        'end_time': '2020-11-19T16:00:00Z',
+        'start_time': '2020-11-18T22:59:59Z',
+        'end_time': '2020-11-18T23:59:59Z',
     }
     response = requests.get(
         url=url,
         params=search_params,
         headers=auth_header,
     )
-    response.raise_for_status()
     print(response.headers)
+    response.raise_for_status()
+    # print(response.headers)
     data = response.json()
-    from pprint import pprint
-    pprint(data)
+    # from pprint import pprint
+    # pprint(data)
     df = pd.DataFrame.from_dict(data['data'])
+    st.write(f'Found {df.shape[0]} tweets so far')
 
     page_count = 1
-    while data.get('meta', {}).get('next_token', None):
+    while data.get('meta', {}).get('next_token', None) and page_count < 10:
         print(f'\tfetching page {page_count}')
         page_count += 1
         next_token = data['meta']['next_token']
@@ -47,16 +49,82 @@ def _get_recent_tweets(token, search_keyword, fields, lang='en', include_retweet
             params=search_params,
             headers=auth_header,
         )
+        print(response.headers)
+        response.raise_for_status()
+        data = response.json()
+        df = df.append(pd.DataFrame.from_dict(data['data']), ignore_index=True)
+        st.write(f'Found {df.shape[0]} tweets so far')
+        time.sleep(5)
+    print(f'Done getting recent tweets, found: {df.shape[0]} tweets')
+    return df
+
+
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+def _get_recent_tweets(token, search_keyword, fields, lang='en', include_retweets=False, duration="Last 24 hours", max_count=50):
+    print('Fetching recent tweets ...')
+    fields_ = set(['text', 'referenced_tweets', 'lang'] + fields)
+    end_time = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    d = {
+        'Last 4 hours': timedelta(hours=4),
+        'Last 8 hours': timedelta(hours=8),
+        'Last 12 hours': timedelta(hours=12),
+        'Last 24 hours': timedelta(hours=24),
+        'Last 48 hours': timedelta(hours=48),
+        'Last 72 hours': timedelta(hours=72),
+    }
+    start_time = (datetime.now() - d[duration]).strftime('%Y-%m-%dT%H:%M:%SZ')
+    url = 'https://api.twitter.com/2/tweets/search/recent'
+    auth_header = {'Authorization': f'Bearer {token}'}
+    search_params = {
+        'query': search_keyword,
+        'max_results': 100,
+        'tweet.fields': ','.join(fields_),
+        'start_time': start_time,
+        'end_time': end_time,
+    }
+    response = requests.get(
+        url=url,
+        params=search_params,
+        headers=auth_header,
+    )
+    # print(response.headers)
+    response.raise_for_status()
+    # print(response.headers)
+    data = response.json()
+    # from pprint import pprint
+    # pprint(data)
+    df = pd.DataFrame.from_dict(data['data'])
+
+    if include_retweets is False:
+        df = df.loc[pd.isna(df['referenced_tweets'])]
+    df = df.loc[df['lang'] == lang]
+
+    st.write(f'Found {df.shape[0]} tweets so far')
+
+    page_count = 1
+    while data.get('meta', {}).get('next_token', None) and df.shape[0] < max_count:
+        print(f'\tfetching page {page_count}')
+        page_count += 1
+        next_token = data['meta']['next_token']
+        search_params['next_token'] = next_token
+        response = requests.get(
+            url=url,
+            params=search_params,
+            headers=auth_header,
+        )
+        print(response.headers)
         response.raise_for_status()
         data = response.json()
         df = df.append(pd.DataFrame.from_dict(data['data']), ignore_index=True)
 
-    print(f'Done getting recent tweets, found: {df.shape[0]} tweets')
-    if include_retweets is False:
-        df = df.loc[pd.isna(df['referenced_tweets'])]
-        print(f'After excluding retweets, found: {df.shape[0]}')
+        if include_retweets is False:
+            df = df.loc[pd.isna(df['referenced_tweets'])]
+        df = df.loc[df['lang'] == lang]
 
-    df = df.loc[df['lang'] == lang]
+        st.write(f'Found {df.shape[0]} tweets so far')
+        time.sleep(5)
+
     print(f'After filtering for language, found: {df.shape[0]} tweets')
 
     if 'referenced_tweets' not in fields:
@@ -82,31 +150,58 @@ def _get_zero_shot_classification(text, labels):
 
 
 def fetch_and_analyze_tweets(
-    token, search_keyword, fields, tweet_language, max_count, include_retweets, zero_shot_categories,
+    token, search_keyword, fields, tweet_language, include_retweets, duration, max_count,
 ):
     if token == "":
         st.write("Please enter a Twitter API bearer token to continue.")
         st.stop()
 
-    candidate_categories = zero_shot_categories
+    if search_keyword == "":
+        st.write("Please enter a search keyword (example: covid)")
+        st.stop()
 
-    df = _get_recent_tweets(token, search_keyword, fields, tweet_language, include_retweets, max_count)
+    candidate_categories = st.text_input(
+        "Comma-separated categories to use for zero-shot classification (example: school,work,travel,health,vacation",
+        "",
+    )
+
+    if candidate_categories == "":
+        st.write("Please enter candidate categories")
+        st.stop()
+
+    st.write(f'Fetching recent tweets related to {search_keyword}')
+    df = _get_recent_tweets(token, search_keyword, fields, tweet_language, include_retweets, duration, max_count)
     
+    st.write(f'Analyzing recent tweets related to {search_keyword}')
     print('Generating histogram of tweets')
     hist = Counter()
     predicted_categories = []
-    for idx, row in df.iterrows():
-        print(f'\tProcessing {idx}')
-        res = _get_zero_shot_classification(row["text"], candidate_categories)
-        if res['scores'][0] > 0.5:
-            predicted_categories.append(res['labels'][0])
-            hist.update({res['labels'][0]: 1})
-        else:
-            predicted_categories.append('unknown')
-            hist.update({'unknown': 1})
+    prediction_confidence = []
 
-    df['predicted_category'] = predicted_categories
-    st.write(hist)
-    st.bar_chart(pd.DataFrame.from_dict(hist, orient='index').reset_index())
-    with st.beta_expander('Expand to see the raw tweets data'):
+    c1, c2 = st.beta_columns(2)
+
+    with c1:
         st.table(df)
+
+    with c2:
+        predictions = _get_zero_shot_classification(
+            df['text'].tolist(),
+            candidate_categories,
+        )
+        print(predictions)
+        for prediction in predictions:
+            labels = prediction['labels']
+            scores = prediction['scores']
+            if scores[0] > 0.5:
+                predicted_categories.append(labels[0])
+                prediction_confidence.append(scores[0])
+                hist.update({labels[0]: 1})
+            else:
+                predicted_categories.append('unknown')
+                prediction_confidence.append(0)
+                hist.update({'unknown': 1})
+
+        df['predicted_category'] = predicted_categories
+        df['prediction_confidence'] = prediction_confidence
+        st.write(hist)
+        st.bar_chart(pd.DataFrame.from_dict(hist, orient='index').reset_index())
